e5c60d2451877a70ca59214e6b1d330f7264f0cd
Thomas M <micheli17u@etu.univ-lorraine.fr>
2024-06-20 13:24:26 +0200

    BRUUUUUUUMA

--- src/MainKerrian.java
+++ src/MainKerrian.java
@@ -6,18 +6,19 @@ public class MainKerrian {
         FlouMoyenne flouMoyenne = new FlouMoyenne();
 
         try {
-            flouMoyenne.appliquerFiltreConvolution("images/Planete 1.jpg");
+            /*flouMoyenne.appliquerFiltreConvolution("images/Planete 1.jpg");
             flouMoyenne.appliquerFiltreConvolution("images/Planete 2.jpg");
             flouMoyenne.appliquerFiltreConvolution("images/Planete 3.jpg");
             flouMoyenne.appliquerFiltreConvolution("images/Planete 4.jpg");
-            flouMoyenne.appliquerFiltreConvolution("images/Planete 5.jpeg");
-            /**
+            flouMoyenne.appliquerFiltreConvolution("images/Planete 5.jpeg");*/
+
             FlouGaussien flouGaussien = new FlouGaussien(7);
             flouGaussien.appliquerFiltreConvolution("./images/Planete 1.jpg");
             KMeans kMeans = new KMeans(10, 10);
             ImageBiomes imageBiomes = new ImageBiomes(kMeans, Palette.DEFAULT);
             imageBiomes.afficherBiomes("images/Gaussien-new-Planete 1.jpg");
-            **/
+
+
             DBSCAN dbscan = new DBSCAN(0.01, 1);
             ImageEcosystemes imageEcosystemes = new ImageEcosystemes(dbscan, Palette.DEFAULT);
             imageEcosystemes.afficherEcosystemes("images/img_1.png");
